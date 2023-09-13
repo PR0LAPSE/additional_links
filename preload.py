d93f52289261fb1aa204b852daaee2f50bdeb9d7
prolapse <117679376+PR0LAPSE@users.noreply.github.com>
2023-09-13 23:29:56 +0500

    Add files via upload

---
+++ preload.py
@@ -0,0 +1,18 @@
+import argparse
+
+
+def preload(parser: argparse.ArgumentParser):
+    
+    parser.add_argument(
+        "--localhostrun",
+        action="store_true",
+        help="use localhost.run, alternative to gradio --share",
+    )
+
+    parser.add_argument(
+        "--remotemoe",
+        action="store_true",
+        help="use remote.moe, alternative to gradio --share",
+    )
+
+    
